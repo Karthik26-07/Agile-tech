171f80ea8bdc47c44d1a2511490910817542f982
Karthik26-07 <118439237+Karthik26-07@users.noreply.github.com>
2022-12-28 14:11:11 +0530

    update

--- Server(AgileTech)/Agiletech/Register&login/Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-session_destroy();
-$data = array('response' => "Logout"); // for invalid login response
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($data);
-?>
